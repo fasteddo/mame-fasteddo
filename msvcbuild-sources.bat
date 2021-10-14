fec29a543ee7a79b2a364feb6a446ee3d8bfd5e8
fasteddo <34660283+fasteddo@users.noreply.github.com>
2021-10-14 13:46:53 -0700

    fixed bug in SOURCES

--- msvcbuild-sources.bat
+++ msvcbuild-sources.bat
@@ -11,7 +11,7 @@ src/mame/audio/llander.cpp,^
 src/mame/drivers/1942.cpp,^
 src/mame/drivers/8080bw.cpp,^
 src/mame/drivers/asteroid.cpp,^
-src/mame/drivers/atarisy2.cpp ^
+src/mame/drivers/atarisy2.cpp,^
 src/mame/drivers/centiped.cpp,^
 src/mame/drivers/dkong.cpp,^
 src/mame/drivers/galaga.cpp,^
