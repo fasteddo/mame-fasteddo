a9f3082d9918b69724b0d6fd55ba7c8df06ecd21
fasteddo <34660283+fasteddo@users.noreply.github.com>
2021-11-20 15:16:52 -0800

    added williams driver to msvcbuild-sources.bat

--- msvcbuild-sources.bat
+++ msvcbuild-sources.bat
@@ -7,7 +7,6 @@
 @rem make SUBTARGET=tiny vs2015 
 @rem make vs2015 SUBTARGET=drivername SOURCES=src\mame\drivers\drivername.cpp
 make vs2019 REGENIE=1 SOURCES=^
-src/mame/audio/llander.cpp,^
 src/mame/drivers/1942.cpp,^
 src/mame/drivers/8080bw.cpp,^
 src/mame/drivers/asteroid.cpp,^
@@ -20,5 +19,6 @@ src/mame/drivers/m52.cpp,^
 src/mame/drivers/mw8080bw.cpp,^
 src/mame/drivers/pacman.cpp,^
 src/mame/drivers/pong.cpp,^
-src/mame/drivers/taitosj.cpp ^
+src/mame/drivers/taitosj.cpp,^
+src/mame/drivers/williams.cpp ^
 PYTHON_EXECUTABLE=d:/msys64-32-2019-12-23/mingw64/bin/python.exe
