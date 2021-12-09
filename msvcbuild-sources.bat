8c7d4eb6f6bae7dbd6093e1ae187c5f8d40a2961
fasteddo <34660283+fasteddo@users.noreply.github.com>
2021-12-08 22:22:19 -0800

    added rallyx.cpp to list of sources

--- msvcbuild-sources.bat
+++ msvcbuild-sources.bat
@@ -20,6 +20,7 @@ src/mame/drivers/m52.cpp,^
 src/mame/drivers/mw8080bw.cpp,^
 src/mame/drivers/pacman.cpp,^
 src/mame/drivers/pong.cpp,^
+src/mame/drivers/rallyx.cpp,^
 src/mame/drivers/taitosj.cpp,^
 src/mame/drivers/williams.cpp ^
 PYTHON_EXECUTABLE=d:/msys64-32-2019-12-23/mingw64/bin/python.exe
