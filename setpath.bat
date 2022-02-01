39200046aa2c6215267b845b9891c3765bc0ac1b
fasteddo <34660283+fasteddo@users.noreply.github.com>
2022-02-01 14:28:26 -0800

    setpath - updated to use latest version of the tools - msys64-2022-01-12

--- setpath.bat
+++ setpath.bat
@@ -2,5 +2,6 @@
 @rem old
 @rem set path=%PATH%;c:\mingw64-w32\bin;c:\mingw64-w32\opt\bin;c:\mingw64-w32\Qt\bin
 @rem set path=%PATH%;d:\msys64;d:\msys64\win32;d:\msys64\mingw64\bin
+@rem set path=%PATH%;d:\msys64-32-2019-12-23;d:\msys64-32-2019-12-23\win32;d:\msys64-32-2019-12-23\mingw32\bin
 
-set path=%PATH%;d:\msys64-32-2019-12-23;d:\msys64-32-2019-12-23\win32;d:\msys64-32-2019-12-23\mingw32\bin
+set path=%PATH%;d:\msys64-2022-01-12;d:\msys64-2022-01-12\win32;d:\msys64-2022-01-12\mingw64\bin
